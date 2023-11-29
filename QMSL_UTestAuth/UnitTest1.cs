4125fbf848d5f4d8c34fea80fb41fdd31491d524
Brain-Ded <vladyslav.atorin.pz.2020@lpnu.ua>
2023-11-29 20:28:18 +0200

    Kek

---
+++ QMSL_UTestAuth/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace QMSL_UTestAuth
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
