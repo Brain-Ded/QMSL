d42550f2965b34dcfd870f80306a1d0255499627
Brain-Ded <vladyslav.atorin.pz.2020@lpnu.ua>
2023-12-01 12:51:53 +0200

    Added Authentication Tests

--- QMSL_UTestAuth/UnitTest1.cs
+++ QMSL_UTestAuth/UnitTest1.cs
@@ -1,18 +1,72 @@
 using NUnit.Framework;
+using QMSL.Services;
 
 namespace QMSL_UTestAuth
 {
     public class Tests
     {
+        AuthHandler handler;
         [SetUp]
         public void Setup()
         {
+            handler = new AuthHandler();
         }
 
         [Test]
-        public void Test1()
+        public void LoginPos()
         {
-            Assert.Pass();
+            string valEmail = "vladyslav.atorin@gmail.com";
+            string valPassword = "password";
+
+            Assert.IsTrue(valEmail!=null && valPassword!=null);
+            
+        }
+        [Test]
+        public void LoginNeg()
+        {
+            string invalEmail = "";
+            string invalPassword = "";
+            Assert.IsTrue(invalEmail.Contains("@") && invalPassword.Length > 5);
+        }
+        [Test]
+        public void LogoutPos()
+        {
+            string valEmail = "vladyslav.atorin@gmail.com";
+            Assert.True(valEmail.Contains("@"));
+        }
+        [Test]
+        public void LogoutNeg()
+        {
+            string invalEmail = "";
+            Assert.True(invalEmail.Contains("@"));
+        }
+        [Test]
+        public void RegisterPos()
+        {
+            string valEmail = "vladyslav.atorin@gmail.com";
+            string valPassword = "password";
+            string valName = "Vladyslav";
+            string valSurname = "Atorin";
+            string valFathername = "Yevhenovich";
+            string valSex = "Male";
+            int valAge = 20;
+            string valPhone = "+380952230893";
+            Assert.IsTrue(valEmail!=null && valPassword!=null && valName!=null && valSurname != null 
+                && valFathername != null && valSex != null && valAge>=0 && (valPhone != null&& valPhone.StartsWith("+")));
+        }
+        [Test]
+        public void RegisterNeg()
+        {
+            string invalEmail = "";
+            string invalPassword = "";
+            string invalName = "";
+            string invalSurname = "Atorin";
+            string invalFathername = "Yevhenovich";
+            string invalSex = "Male";
+            int invalAge = -20;
+            string invalPhone = "324234";
+            Assert.IsTrue(invalEmail!=null && invalPassword!=null && invalName!=null && invalSurname != null
+                && invalFathername != null && invalSex != null && invalAge>=0 && invalPhone != null);
         }
     }
 }
--- Services/AuthHandler.cs
+++ Services/AuthHandler.cs
@@ -4,7 +4,7 @@
     public class AuthHandler
     {
         public void Login(string Email, string Password) { throw new NotImplementedException(); }
-        public void Logout() { throw new NotImplementedException(); }
+        public void Logout(string Email) { throw new NotImplementedException(); }
         public void Register(string Email, string Password, int Age, string Sex, string PhoneNumber, 
             string Name, string Surname, string? FatherName = null) 
         { throw new NotImplementedException(); }
