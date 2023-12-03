79d7a4fdd89c3133be1ebecc770e76f8f434149e
Brain-Ded <vladyslav.atorin.pz.2020@lpnu.ua>
2023-12-03 20:24:04 +0200

    AddedServices

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -2,12 +2,20 @@
 
 namespace QMSL.Models
 {
-    public class Comment
+    public partial class Comment
     {
         public int Id { get; set; }
         public int DoctorId { get; set; }
         public string Text { get; set; }
         public CommentTypes type { get; set; }
         public DateTime CommentedAt { get; set; }
+    }
+
+    public partial class Comment
+    {
+        public void lol()
+        {
+           
+        }
     }
 }
---
+++ Services/StatisticService.cs
@@ -0,0 +1,22 @@
+﻿namespace QMSL.Services
+{
+    public class StatisticService
+    {
+        public void GetPatientPollStatistic(int pId)
+        {
+
+        }
+        public void GetPatientPollStatistic(int pId, DateTime from, DateTime to)
+        {
+
+        }
+        public void GetDoctorStatistic(int dId)
+        {
+
+        }
+        public void GetDoctorStatistic(int dId, DateTime from, DateTime to)
+        {
+
+        }
+    }
+}
